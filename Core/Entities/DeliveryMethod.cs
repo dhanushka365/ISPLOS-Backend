fc0bf5e35ad7df10380c94305e730b6fc2e76833
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-25 13:37:50 +0530

    works commited

---
+++ Core/Entities/DeliveryMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class DeliveryMethod : BaseEntity
+    {
+        public string ShortName { get; set; } // 1. Add this property
+        public string DeliveryTime { get; set; } // 2. Add this property
+        public string Description { get; set; } // 3. Add this property
+        public decimal Price { get; set; } // 4. Add this property
+    }
+}
