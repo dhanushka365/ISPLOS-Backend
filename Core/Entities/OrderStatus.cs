04822426d74f9bda9f22095d6f623841e56cc7cf
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-25 14:28:39 +0530

    works commited

--- Core/Entities/OrderStatus.cs
+++ Core/Entities/OrderStatus.cs
@@ -9,5 +9,23 @@ namespace Core.Entities
     public class OrderStatus :BaseEntity
     {
         public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public string StatusCode { get; set; }
+
+        public string StatusMessage { get; set; }
+
+        public string StatusType { get; set; }
+
+        public string StatusDescription { get; set; }
+
+        public string StatusTypeCode { get; set; }
+
+        public string StatusTypeMessage { get; set; }
+
+        public string StatusTypeDescription { get; set; }
+
+
     }
 }
--- Core/Entities/ProductBrand.cs
+++ Core/Entities/ProductBrand.cs
@@ -9,5 +9,7 @@ namespace Core.Entities
     public class ProductBrand :BaseEntity
     {
             public string Name { get; set; }
+            public string Description { get; set; }
+
     }
 }
