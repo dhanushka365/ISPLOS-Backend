2d78c3141f767ce9992251da801eb114c9ba8bc5
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-26 15:41:07 +0530

    works commited

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,13 @@ namespace Core.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(Expression<Func<T, bool>> predicate);
 
         Task<IReadOnlyList<T>> ListAllAsync();
 
-        Task DeleteByIdAsync(int id);
+        Task DeleteByIdAsync(Expression<Func<T, bool>> predicate);
 
-        Task UpdateByAsync(int id);
+        Task UpdateByAsync(Expression<Func<T, bool>> predicate);
 
         //Task<T> GetEntityWithSpec(ISpecification<T> spec);
         //Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
