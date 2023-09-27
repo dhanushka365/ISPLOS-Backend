d2d4fbb31e1ca240e3a02fa5569fbd6a91f13bc9
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-27 11:05:16 +0530

    works commited

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq.Expressions;
@@ -19,17 +20,17 @@ namespace API.Controllers
         }
 
 
-        //[HttpGet("brands")]
-        //public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands(
+        //[HttpGet("Orders")]
+        //public async Task<ActionResult<IReadOnlyList<Order>>> GetProductBrands(
         //     [FromQuery] string search = null,
         //     [FromQuery] int page = 1,
         //     [FromQuery] int pageSize = 10)
         //{
 
-        //    Expression<Func<ProductBrand, bool>> filter = null;
+        //    Expression<Func<Order, bool>> filter = null;
         //    if (!string.IsNullOrEmpty(search))
         //    {
-        //        filter = brand => brand.Name.Contains(search, StringComparison.OrdinalIgnoreCase);
+        //        filter = order => order.BuyerEmail.Contains(search, StringComparison.OrdinalIgnoreCase);
         //    }
 
 
