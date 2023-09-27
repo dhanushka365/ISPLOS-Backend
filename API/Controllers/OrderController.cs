32594006c0b6b57765585a536b216b66cbb8a09c
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-27 10:09:09 +0530

    works commited

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Core.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 
 namespace API.Controllers
 {
@@ -16,7 +18,33 @@ namespace API.Controllers
             this.mapper = mapper;
         }
 
-       
+
+        //[HttpGet("brands")]
+        //public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands(
+        //     [FromQuery] string search = null,
+        //     [FromQuery] int page = 1,
+        //     [FromQuery] int pageSize = 10)
+        //{
+
+        //    Expression<Func<ProductBrand, bool>> filter = null;
+        //    if (!string.IsNullOrEmpty(search))
+        //    {
+        //        filter = brand => brand.Name.Contains(search, StringComparison.OrdinalIgnoreCase);
+        //    }
+
+
+        //    int skip = (page - 1) * pageSize;
+
+
+        //    var brands = await _productBrandRepo.ListAllAsync(
+        //        filter: filter,
+        //        orderBy: null,
+        //        pageNumber: page,
+        //        pageSize: pageSize);
+
+        //    return Ok(brands);
+        //    //return Ok(await _productBrandRepo.ListAllAsync());
+        //}
 
 
     }
