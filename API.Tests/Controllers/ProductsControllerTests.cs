528351e7cec6f524b05a5e0336d89277568df345
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-29 12:39:45 +0530

    project build without errors

--- API.Tests/Controllers/ProductsControllerTests.cs
+++ API.Tests/Controllers/ProductsControllerTests.cs
@@ -1,11 +1,30 @@
+using API.Controllers;
+using AutoFixture;
+using Core.Entities;
+using Moq;
+
 namespace API.Tests.Controllers
 {
     public class ProductsControllerTests
     {
+
+        private readonly IFixture _fixture;
+        private readonly Mock<Product> _serviceMockProduct;
+        private readonly ProductsController _productController;
+
+        public ProductsControllerTests(IFixture fixture, Mock<Product> serviceMockProduct, ProductsController productController)
+        {
+            _fixture = fixture;
+            _serviceMockProduct = serviceMockProduct;
+            _productController = productController;
+        }
+
         [Fact]
         public void Test1()
         {
+            
 
         }
+    
     }
 }
