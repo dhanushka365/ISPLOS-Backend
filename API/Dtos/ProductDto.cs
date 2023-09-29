da11327bc1f47f10b5caca0e91bc8767e58e93d0
DasunPathirajaICP <dpathiraja@icptechno.com>
2023-09-29 10:30:03 +0530

    Update Product Controller and Fixed the Issue

--- API/Dtos/ProductDto.cs
+++ API/Dtos/ProductDto.cs
@@ -20,11 +20,11 @@ namespace API.Dtos
         [Required]
         public string PictureUrl { get; set; }
 
-        [Required]
-        public Guid ProductTypeId { get; set; }
+        //[Required]
+        //public Guid ProductTypeId { get; set; }
 
-        [Required]
-        public Guid ProductBrandId { get; set; }
+        //[Required]
+        //public Guid ProductBrandId { get; set; }
 
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
